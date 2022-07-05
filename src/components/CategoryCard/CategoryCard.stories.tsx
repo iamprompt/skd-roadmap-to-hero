345ec60d46319bf09538a99e895b795e89c49e0a
Ratchitta Panyangam <ratchitta.panyangam@gmail.com>
2022-07-05 17:12:18 +0700

    add storybook files

---
+++ src/components/CategoryCard/CategoryCard.stories.tsx
@@ -0,0 +1,10 @@
+import CategoryCard from './CategoryCard'
+
+export default {
+  title: 'Components/CategoryCard',
+  component: CategoryCard,
+}
+
+export const Default = () => {
+  return <CategoryCard />
+}
---
+++ src/components/ProductCard/ProductCard.stories.tsx
@@ -0,0 +1,10 @@
+import ProductCard from './ProductCard'
+
+export default {
+  title: 'Components/ProductCard',
+  component: ProductCard,
+}
+
+export const Default = () => {
+  return <ProductCard />
+}
