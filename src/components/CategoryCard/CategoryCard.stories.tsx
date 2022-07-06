ce98c07b539c989195e9d5e0f749adc040408078
Ratchitta Panyangam <ratchitta.panyangam@gmail.com>
2022-07-06 14:02:57 +0700

    add multiple category doc

--- src/components/CategoryCard/CategoryCard.stories.tsx
+++ src/components/CategoryCard/CategoryCard.stories.tsx
@@ -17,3 +17,32 @@ Default.args = {
   categoryName: 'Data',
   description: 'เรียนรู้และฝึกฝนกระบวนการคิดสร้างมูลค่าให้ธุรกิจด้วยข้อมูล',
 }
+
+export const MultipleCategoryCard = () => (
+  <div className="flex flex-row space-x-4">
+    <CategoryCard
+      linkUrl="#"
+      imgUrl="/images/data-icon.png"
+      categoryName="Data"
+      description="เรียนรู้และฝึกฝนกระบวนการคิดสร้างมูลค่าให้ธุรกิจด้วยข้อมูล"
+    />
+    <CategoryCard
+      linkUrl="#"
+      imgUrl="/images/data-icon.png"
+      categoryName="Technology"
+      description="เรียนรู้และฝึกฝนกระบวนการคิดสร้างมูลค่าให้ธุรกิจด้วยข้อมูล"
+    />
+    <CategoryCard
+      linkUrl="#"
+      imgUrl="/images/data-icon.png"
+      categoryName="Design"
+      description="เรียนรู้และฝึกฝนกระบวนการคิดสร้างมูลค่าให้ธุรกิจด้วยข้อมูล"
+    />
+    <CategoryCard
+      linkUrl="#"
+      imgUrl="/images/data-icon.png"
+      categoryName="Business"
+      description="เรียนรู้และฝึกฝนกระบวนการคิดสร้างมูลค่าให้ธุรกิจด้วยข้อมูล"
+    />
+  </div>
+)
