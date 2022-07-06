2cefc4cb7a4a3006416a72a76158b2eb9acf1519
Ratchitta Panyangam <ratchitta.panyangam@gmail.com>
2022-07-06 21:27:38 +0700

    add mock category, job, course data

---
+++ src/components/utils/mockCourseTech.ts
@@ -0,0 +1,62 @@
+export const mockCourseData = [
+  {
+    imgUrl:
+      'https://public-assets.skooldio.com/storefront-sku/b14a037f-2c98-4bae-b216-1cb383ffd634/basics-web.png',
+    title: 'Web Basics with HTML and CSS',
+    info: 'ก้าวแรกสู่การพัฒนาเว็บไซต์ด้วย HTML และ CSS',
+    hour: '6-8 ชั่วโมง',
+    instructor: 'ทัศน์พล รัชตะสัมฤทธิ์',
+    instructorImgUrl:
+      'https://public-assets.skooldio.com/storefront-category%2Ffbe4cd4b-ec0a-4b2c-9c7e-a119cf28cf39%2Ftadpol-rachatasumrit.jpeg',
+  },
+  {
+    imgUrl:
+      'https://public-assets.skooldio.com/storefront-sku/bc16ec81-283a-4dc3-b763-7e0421401281/essential-css.png',
+    title: 'Essential CSS for Web Developers',
+    info: 'ปรับแต่งและเพิ่มลูกเล่นให้เว็บไซต์ด้วย CSS',
+    hour: '8-10 ชั่วโมง',
+    instructor: 'ทัศน์พล รัชตะสัมฤทธิ์',
+    instructorImgUrl:
+      'https://public-assets.skooldio.com/storefront-category%2Ffbe4cd4b-ec0a-4b2c-9c7e-a119cf28cf39%2Ftadpol-rachatasumrit.jpeg',
+  },
+  {
+    imgUrl:
+      'https://public-assets.skooldio.com/storefront-sku/0f74ea90-6781-4fb7-8ffb-0effe99e8b30/modern-javascript-es6.png',
+    title: 'Modern JavaScript (ES6)',
+    info: 'พื้นฐาน ES6 ที่จำเป็นสำหรับการเขียน JavaScript',
+    hour: '2-3 ชั่วโมง',
+    instructor: 'Skooldio Team',
+    instructorImgUrl:
+      'https://public-assets.skooldio.com/storefront-category%2F6bae3f47-a202-42ec-883f-1b5c459d401a%2FSkooldio_logo_crop.jpeg',
+  },
+  {
+    imgUrl:
+      'https://public-assets.skooldio.com/storefront-sku/0edbd0e1-aabf-439a-a5f7-d19c43718665/Git%20Essentials.png',
+    title: 'Git Essentials for Developers',
+    info: 'พื้นฐานแน่น แม่นคำสั่ง เรียนรู้การใช้ Git อย่างมืออาชีพ',
+    hour: '3 ชั่วโมง',
+    instructor: 'ตรัยรัตน์ ปัญญาวัฒนานุกูล',
+    instructorImgUrl:
+      'https://public-assets.skooldio.com/storefront-category%2Fa23f4cc7-b9db-4caf-b4e7-e7908a47c35e%2FScreen%2520Shot%25202565-06-11%2520at%252021.33.19.png',
+  },
+  {
+    imgUrl:
+      'https://public-assets.skooldio.com/storefront-sku/c3f1b0b0-df54-4538-beea-31f298fbc0ee/react-cover.png',
+    title: 'Mastering Web Development with React',
+    info: 'เริ่มพัฒนาเว็บแอปพลิเคชันด้วย React อย่างถูกวิธี',
+    hour: '4-6 ชั่วโมง',
+    instructor: 'ปัญจมพงศ์ เสริมสวัสดิ์ศรี',
+    instructorImgUrl:
+      'https://public-assets.skooldio.com/storefront-category%2Ff41bc743-718f-4ccb-a4c2-5283caa32fdd%2Fpanj.jpg',
+  },
+  {
+    imgUrl:
+      'https://public-assets.skooldio.com/storefront-sku/3637e888-e65f-4b47-84fd-b8b96697eb8a/react-hooks.jpeg',
+    title: 'React Hooks in Action',
+    info: 'พัฒนาแอปพลิเคชันอย่างมีประสิทธิภาพด้วย React hook',
+    hour: '2-3 ชั่วโมง',
+    instructor: 'ปัญจมพงศ์ เสริมสวัสดิ์ศรี',
+    instructorImgUrl:
+      'https://public-assets.skooldio.com/storefront-category%2Ff41bc743-718f-4ccb-a4c2-5283caa32fdd%2Fpanj.jpg',
+  },
+]
---
+++ src/components/utils/mockJobData.ts
@@ -0,0 +1,23 @@
+export const mockJobData = [
+  {
+    jobName: 'Data Analyst',
+    jobDescription:
+      'ทำงานวิเคราะห์ Big Data หรือข้อมูลจำนวนมหาศาล แล้วนำมา Visualized เพื่อสรุปหา insight ที่จะสร้างข้อได้เปรียบทางธุรกิจ',
+    jobIcon: '/images/job-icon/psychology.png',
+    jobLink: '#',
+  },
+  {
+    jobName: 'Data Scientist',
+    jobDescription:
+      'ทำงานวิเคราะห์ Big Data โดยใช้หลักการทางคณิตศาสตร์เพื่อให้สามารถคาด การณ์ได้จาก Data ที่มีผ่าน Machine Learning',
+    jobIcon: '/images/job-icon/science.png',
+    jobLink: '#',
+  },
+  {
+    jobName: 'Data Engineer',
+    jobDescription:
+      'ทำงานจัดเก็บ รวบรวม Big Data ให้เป็นระเบียบ มีคุณภาพ และพร้อมอยู่ใน format ที่จะสามารถส่งไปเพื่อวิเคราะห์หรือใช้งานต่อ',
+    jobIcon: '/images/job-icon/engineering.png',
+    jobLink: '#',
+  },
+]
