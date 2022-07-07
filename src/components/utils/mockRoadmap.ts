c1911403a880504518b3b4d2e41ff4e80ae6f811
Ratchitta Panyangam <ratchitta.panyangam@gmail.com>
2022-07-08 02:01:33 +0700

    add roadmap

---
+++ src/components/utils/mockRoadmap.ts
@@ -0,0 +1,32 @@
+export const mockRoadmap = [
+  {
+    state: 'ACTIVE',
+    isLeft: true,
+    courseImgUrl:
+      'https://public-assets.skooldio.com/storefront-sku%2F49c114c6-3bf5-4221-bba5-12638000ed71%2Fweb-scraping.png',
+  },
+  {
+    state: 'DISABLE',
+    isLeft: false,
+    courseImgUrl:
+      'https://public-assets.skooldio.com/storefront-sku%2F39c40c6a-0810-4e67-8cb7-ac2b9e9e0656%2Fsql-with-logo.png',
+  },
+  {
+    state: 'LOCKED',
+    isLeft: true,
+    courseImgUrl:
+      'https://public-assets.skooldio.com/storefront-sku%2F5c6a9d4e-d1c8-48fd-9571-578645ead59d%2Feda.png',
+  },
+  {
+    state: 'COMPLETE',
+    isLeft: false,
+    courseImgUrl:
+      'https://public-assets.skooldio.com/storefront-sku%2Fc31472b1-c9c4-417e-984d-cd0cd13bf3ea%2Fpowerbi.png',
+  },
+  {
+    state: 'DISABLE',
+    isLeft: true,
+    courseImgUrl:
+      'https://public-assets.skooldio.com/storefront-sku%2F87ed6d56-5524-4f98-8183-6002b8a6160c%2FHands-on-Power-BI.jpg',
+  },
+]
