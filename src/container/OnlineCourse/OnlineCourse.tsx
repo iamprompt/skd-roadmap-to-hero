2e6907cb1843ca3db97de1543a338397c8274ac3
Supakarn Laorattanakul <wearedprompt@gmail.com>
2022-07-07 15:30:20 +0700

    change to guides

--- src/container/OnlineCourse/OnlineCourse.tsx
+++ src/container/OnlineCourse/OnlineCourse.tsx
@@ -4,7 +4,7 @@ import type { ProductCardProps } from 'src/apps/components/ProductCard/types'
 const OnlineCourse = (courses: ProductCardProps[]) => {
   return (
     <div className="mt-20 mb-20 grid justify-center gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:pl-80 2xl:pr-80">
-      {courses.courses.map((course) => {
+      {courses.map((course) => {
         const { imgUrl, title, info, hour, instructor, instructorImgUrl } =
           course || {}
         return (
