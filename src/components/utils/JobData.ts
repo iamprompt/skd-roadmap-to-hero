22985185f75b0d2591e7cec2ce9604b865384477
Supakarn Laorattanakul <wearedprompt@gmail.com>
2022-07-08 13:19:10 +0700

    add job data mock example

---
+++ src/components/utils/JobData.ts
@@ -0,0 +1,10 @@
+export const JobData = [
+  {
+    title: 'Data Analyst',
+    type: 'Full Time',
+    company: 'AIS',
+    companyLogo: '',
+    salary: '25,000 - 35,000',
+    location: 'Bangkok',
+  },
+]
