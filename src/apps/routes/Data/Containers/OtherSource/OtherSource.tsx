2de588d2d9a34cbfd346b4ec1cc0619f8b63382e
Ratchitta Panyangam <ratchitta.panyangam@gmail.com>
2022-07-08 13:56:15 +0700

    justify other source center

--- src/apps/routes/Data/Containers/OtherSource/OtherSource.tsx
+++ src/apps/routes/Data/Containers/OtherSource/OtherSource.tsx
@@ -2,7 +2,7 @@ const OtherSource = () => {
   return (
     <div className="mx-auto max-w-screen-xl py-10 px-8">
       <p className="mb-1 text-4xl font-medium">แหล่งข้อมูลอื่นๆ</p>
-      <div className="flex">
+      <div className="flex justify-center">
         <div className="mr-4 w-[272px] text-center">
           <img className="mx-auto" src="/images/data-job/tutorials.svg" />
           <p className="my-3 text-2xl font-semibold">Skooldio Tutorials</p>
